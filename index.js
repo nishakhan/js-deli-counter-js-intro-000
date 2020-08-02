a3b26233ceb221226db340e7ed57ca49554cccd5
Nisha Khan <nisha.f.khan@gmail.com>
2020-08-02 16:13:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function currentLine(deliLine) {
   } else {
     var callNames = "The line is currently: "
       for (let i=0; i < deliLine.length; i++) {
-        return callNames += ++i + ". " + deliLine[i];
+        callNames += ++i + ". " + deliLine[i];
       }
+    return callNames
     }
 }
