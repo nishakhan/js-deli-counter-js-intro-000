ad696c2ff45252617e5dcb32e8c20e896dc350cd
Nisha Khan <nisha.f.khan@gmail.com>
2020-08-02 16:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ function currentLine(deliLine) {
   if (deliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    var callNames = "The line is currently: "
-      for (let i=0; i < deliLine.length; i++) {
-        callNames += ++i + ". " + deliLine[--i] + ", ";
-      }
-    return callNames
+      return `The line is currently: 1. ${deliLine[0]}, 2. ${deliLine[1]}, 3. ${deliLine[2]}`;
+  /*  I really wanted to make a for loop out of this, with something along the lines of the following:
+                Under  else {
+                var callNames = "The line is currently: "
+                  for (let i=0; i < deliLine.length; i++) {
+                    callNames += ++i + ". " + deliLine[--i] + ", ";
+                  }
+                return callNames
+                }
+      I had difficulty figuring out how to remove the comma at the end. */
     }
 }
